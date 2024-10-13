07ab7a2aeddf34ad667e9b0873c2fca59eadae21
xd <vavassorivalen@gmail.com>
2024-10-12 21:37:18 -0300

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,4 +38,6 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+
 }
